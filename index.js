6819240de72489b25e6e0737248e144a5bf2a332
hellbird <hellbird@MacBook-Pro.local>
2022-07-11 18:26:43 +0900

    init

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.RouteLoader = exports.METHODS = void 0;
 
-var _urlJoin = _interopRequireDefault(require("url-join"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
 
 function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
@@ -28,6 +24,75 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function normalize(strArray) {
+  var resultArray = [];
+
+  if (strArray.length === 0) {
+    return '';
+  }
+
+  if (typeof strArray[0] !== 'string') {
+    throw new TypeError('Url must be a string. Received ' + strArray[0]);
+  } // If the first part is a plain protocol, we combine it with the next part.
+
+
+  if (strArray[0].match(/^[^/:]+:\/*$/) && strArray.length > 1) {
+    strArray[0] = strArray.shift() + strArray[0];
+  } // There must be two or three slashes in the file protocol, two slashes in anything else.
+
+
+  if (strArray[0].match(/^file:\/\/\//)) {
+    strArray[0] = strArray[0].replace(/^([^/:]+):\/*/, '$1:///');
+  } else {
+    strArray[0] = strArray[0].replace(/^([^/:]+):\/*/, '$1://');
+  }
+
+  for (var i = 0; i < strArray.length; i++) {
+    var component = strArray[i];
+
+    if (typeof component !== 'string') {
+      throw new TypeError('Url must be a string. Received ' + component);
+    }
+
+    if (component === '') {
+      continue;
+    }
+
+    if (i > 0) {
+      // Removing the starting slashes for each component but the first.
+      component = component.replace(/^[\/]+/, '');
+    }
+
+    if (i < strArray.length - 1) {
+      // Removing the ending slashes for each component but the last.
+      component = component.replace(/[\/]+$/, '');
+    } else {
+      // For the last component we will combine multiple slashes to a single one.
+      component = component.replace(/[\/]+$/, '/');
+    }
+
+    resultArray.push(component);
+  }
+
+  var str = resultArray.join('/'); // Each input component is now separated by a single slash except the possible first plain protocol part.
+  // remove trailing slash before parameters or hash
+
+  str = str.replace(/\/(\?|&|#[^!])/g, '$1'); // replace ? in parameters with &
+
+  var parts = str.split('?');
+  str = parts.shift() + (parts.length > 0 ? '?' : '') + parts.join('&');
+  return str;
+}
+
+function urlJoin() {
+  for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+
+  var parts = Array.from(Array.isArray(args[0]) ? args[0] : args);
+  return normalize(parts);
+}
 
 var METHODS = {
   GET: 'get',
@@ -77,13 +142,13 @@ var RouteLoader = /*#__PURE__*/function () {
             action = node.action,
             before = node.before;
         if (typeof subPath !== 'string') continue;
-        var fullPath = (0, _urlJoin["default"])('/', rootPath, subPath);
+        var fullPath = urlJoin('/', rootPath, subPath);
         var middlewares = [].concat(_toConsumableArray(rootMiddleware), _toConsumableArray(middleware));
 
         if (action) {
           (function () {
             console.log("[".concat(method.toUpperCase(), "] registered:"), fullPath);
-            var xaction = typeof action === 'string' ? require(action)["default"] : action;
+            var xaction = action;
 
             if (_this.mixins) {
               _this.mixins.forEach(function (mixin) {
